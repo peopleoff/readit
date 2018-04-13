0e44f0c1e07ade1ef864d14dedb317c802d43211
Peopleoff <12345allmsn>
2018-04-13 14:40:08 -0400

    error handling

--- index.js
+++ index.js
@@ -9,23 +9,39 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.content.substring(0,1) === '!') {
-        let subreddit = msg.content.substring(1).trim().toLocaleLowerCase();
-        let url = `https://www.reddit.com/r/${subreddit}/.json?limit=50`;
+        // replace all "!"
+        let message = msg.content.trim().toLocaleLowerCase();
+        console.log("Full Message: " + message);
+        let subreddit = msg.content.replace(/!/g, " ").trim().toLocaleLowerCase().split(' ')[0];
+        console.log("Subreddit: " + subreddit);
+        let url = `https://www.reddit.com/r/${subreddit}/.json`;
         axios
             .get(url)
             .then(response =>{
-                let dataset = response.data.data.children;
-                let length = response.data.data.children.length;
-                let item = Math.floor(Math.random() * length) + 1;
-                msg.channel.send(dataset[item].data.url);
+                if(response.data.data.children === undefined || response.data.data.children.length === 0 || response.data.data.children[0].data.subreddit_id === undefined){
+                    msg.channel.send("Not a subreddit you idiot");
+                }else {
+                    let dataset = response.data.data.children;
+                    let randomIndex = Math.floor(Math.random() * dataset.length) + 1;
+                    let post = dataset[randomIndex].data;
+                    let image = post.url;
+                    let subreddit = post.subreddit_name_prefixed;
+                    let subredditID = post.subreddit_id;
+
+                    if(subredditID === undefined){
+                        msg.channel.send("Not a subreddit you idiot");
+                    }else{
+                        msg.channel.send(image);
+                    }
+
+                }
             })
             .catch(error => {
-                console.log(error)
+                msg.channel.send("You broke it.");
+                console.log('There was an error.');
             });
 
 
     }
 });
-
-
 client.login(auth.token);
