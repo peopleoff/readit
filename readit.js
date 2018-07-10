712c51860f7c7389c58940cc8a4eb42dd9613e63
peopleoff <Allmsn12!>
2018-07-09 20:13:43 -0400

    added validation for private subreddits

--- readit.js
+++ readit.js
@@ -8,40 +8,39 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content.substring(0,1) === '!') {
+    if (msg.content.substring(0, 1) === '!') {
         // replace all "!"
-        let message = msg.content.trim().toLocaleLowerCase();
-        console.log("Full Message: " + message);
         let subreddit = msg.content.replace(/!/g, " ").trim().toLocaleLowerCase().split(' ')[0];
-        console.log("Subreddit: " + subreddit);
         let url = `https://www.reddit.com/r/${subreddit}/.json`;
         axios
             .get(url)
-            .then(response =>{
-                if(response.data.data.children === undefined || response.data.data.children.length === 0 || response.data.data.children[0].data.subreddit_id === undefined){
-                    msg.channel.send("Not a subreddit you idiot");
-                }else {
+            .then(response => {
+                let res = response.data.data;
+                if (res.children.length === 0) {
+                    msg.channel.send("Not a subreddit");
+                } else {
                     let dataset = response.data.data.children;
                     let randomIndex = Math.floor(Math.random() * dataset.length) + 1;
                     let post = dataset[randomIndex].data;
                     let image = post.url;
-                    let subreddit = post.subreddit_name_prefixed;
                     let subredditID = post.subreddit_id;
 
-                    if(subredditID === undefined){
-                        msg.channel.send("Not a subreddit you idiot");
-                    }else{
+                    if (subredditID === undefined) {
+                        msg.channel.send("Not a subreddit");
+                    } else {
                         msg.channel.send(image);
                     }
-
                 }
             })
             .catch(error => {
-                msg.channel.send("You broke it.");
-                console.log('There was an error.');
+                if (error.response.status === 404) {
+                    msg.channel.send("404");
+                } else {
+                    msg.channel.send(`error: ${error.response.error}, message: ${error.response.message}, reason: ${error.response.reason}`);
+                }
             });
-
-
     }
 });
+
+
 client.login(auth.token);
